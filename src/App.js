3f32cce24cb9e4c58158a10c948d25cbe0431ebd
Ana Sadeghian <asadeghi@hiiakasessence.org>
2025-11-11 21:05:00 -0500

    Fix Vercel build: CI=false and silence unused vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { OpenAI } from "openai";
 import { motion, AnimatePresence } from "framer-motion";
