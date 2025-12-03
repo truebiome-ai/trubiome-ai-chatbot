a2e7307d137431597aa8f5a69ee629f3252b2279
truebiome-ai <asadeghi@hiiakasessence.org>
2025-12-02 20:16:15 -0500

    Jesus Saves

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-import 'dotenv/config';
 import express from "express";
 import cors from "cors";
-
+import "dotenv/config";
 import recommendRouter from "./api/recommend.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// All API endpoints here:
+// Test route
+app.get("/", (req, res) => {
+  res.send("TruBiome backend is running.");
+});
+
+// API route
 app.use("/api/recommend", recommendRouter);
 
 const PORT = process.env.PORT || 3001;
