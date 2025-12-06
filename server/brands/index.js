9f4808dff656ed5d3c8b1b038b224fa974a3b14d
truebiome-ai <asadeghi@hiiakasessence.org>
2025-12-06 09:40:15 -0500

    Jesus saves, the devil shall not dwell here

--- server/brands/index.js
+++
@@ -1,9 +0,0 @@
-import universal from "./universal.js";
-
-const brandMap = {
-  universal,
-};
-
-export function getBrandConfig(name = "universal") {
-  return brandMap[name] || brandMap.universal;
-}
